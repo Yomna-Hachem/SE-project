143eb4e34ae3ce252ec25fb9c8328ccae2637462
Monica-hany <21p0173@eng.asu.edu.eg>
2023-05-17 21:12:38 +0300

    PediatricDentistry

--- SW-Project(Backend)/SW_Project/TheKiddoDontist/urls.py
+++ SW-Project(Backend)/SW_Project/TheKiddoDontist/urls.py
@@ -3,4 +3,5 @@ from .import views
 urlpatterns = [
     path('', views.home,name='TheKiddoDontist-home'),
     path('aboutus', views.aboutus,name='TheKiddoDontist-aboutus'),
+    path('PediatricDentistry', views.PediatricDentistry,name='TheKiddoDontist-PediatricDentistry'),
 ]
