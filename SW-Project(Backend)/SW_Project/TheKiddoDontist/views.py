fe0c781807b95f37431d2199695665479bbf2649
farida-waleed02 <faridawaleed312@hotmail.com>
2023-05-17 20:58:38 +0300

    About us

--- SW-Project(Backend)/SW_Project/TheKiddoDontist/views.py
+++ SW-Project(Backend)/SW_Project/TheKiddoDontist/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'TheKiddoDontist/homepage.html')
+def aboutus(request):
+    return render(request,'TheKiddoDontist/7anafy.html')
