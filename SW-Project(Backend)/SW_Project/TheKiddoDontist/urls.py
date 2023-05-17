d59a93939476f959bde9954ebdc8155a7841b301
farida-waleed02 <faridawaleed312@hotmail.com>
2023-05-17 21:16:25 +0300

    Online Booking

--- SW-Project(Backend)/SW_Project/TheKiddoDontist/urls.py
+++ SW-Project(Backend)/SW_Project/TheKiddoDontist/urls.py
@@ -3,4 +3,5 @@ from .import views
 urlpatterns = [
     path('', views.home,name='TheKiddoDontist-home'),
     path('aboutus', views.aboutus,name='TheKiddoDontist-aboutus'),
+    path('Online Booking', views.onlineBooking,name='TheKiddoDontist-OnlineBooking'),
 ]
--- SW-Project(Backend)/SW_Project/TheKiddoDontist/views.py
+++ SW-Project(Backend)/SW_Project/TheKiddoDontist/views.py
@@ -4,3 +4,5 @@ def home(request):
     return render(request,'TheKiddoDontist/homepage.html')
 def aboutus(request):
     return render(request,'TheKiddoDontist/7anafy.html')
+def onlineBooking(request):
+     return render(request,'TheKiddoDontist/OnlineBooking.html')
