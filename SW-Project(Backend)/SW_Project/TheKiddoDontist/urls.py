3a6bcc5a82782de86484b02f170a3f9c117ab5ed
farida-waleed02 <faridawaleed312@hotmail.com>
2023-05-18 00:28:30 +0300

    Farida

--- SW-Project(Backend)/SW_Project/TheKiddoDontist/urls.py
+++ SW-Project(Backend)/SW_Project/TheKiddoDontist/urls.py
@@ -3,14 +3,11 @@ from .import views
 urlpatterns = [
     path('', views.home,name='TheKiddoDontist-home'),
     path('aboutus', views.aboutus,name='TheKiddoDontist-aboutus'),
-<<<<<<< HEAD
-    path('Online Booking', views.onlineBooking,name='TheKiddoDontist-OnlineBooking'),
-=======
     path('PediatricDentistry', views.PediatricDentistry,name='TheKiddoDontist-PediatricDentistry'),
     path('GeneralAnesthesia', views.GeneralAnesthesia,name='TheKiddoDontist-GeneralAnesthesia'),
     path('contactus', views.contactus,name='TheKiddoDontist-contactus'),
     path('OnlineBooking', views.OnlineBooking,name='TheKiddoDontist-OnlineBooking'),
     path('Reviews', views.Reviews,name='TheKiddoDontist-Reviews'),
     path('login', views.login,name='TheKiddoDontist-login'),
->>>>>>> aad6f5efa8b50ca02b1f33d85f02eec9f8701658
+
 ]
