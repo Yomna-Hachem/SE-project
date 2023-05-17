9dfb8e95f4e6b70b41ccf6422500a8040a66db8c
Monica-hany <21p0173@eng.asu.edu.eg>
2023-05-17 23:11:59 +0300

    Update

--- SW-Project(Backend)/SW_Project/TheKiddoDontist/urls.py
+++ SW-Project(Backend)/SW_Project/TheKiddoDontist/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('aboutus', views.aboutus,name='TheKiddoDontist-aboutus'),
     path('PediatricDentistry', views.PediatricDentistry,name='TheKiddoDontist-PediatricDentistry'),
     path('GeneralAnesthesia', views.GeneralAnesthesia,name='TheKiddoDontist-GeneralAnesthesia'),
+    path('contactus', views.contactus,name='TheKiddoDontist-contactus'),
+    path('OnlineBooking', views.OnlineBooking,name='TheKiddoDontist-OnlineBooking'),
+    path('Reviews', views.Reviews,name='TheKiddoDontist-Reviews'),
+    path('login', views.login,name='TheKiddoDontist-login'),
 ]
--- SW-Project(Backend)/SW_Project/TheKiddoDontist/views.py
+++ SW-Project(Backend)/SW_Project/TheKiddoDontist/views.py
@@ -8,4 +8,12 @@ def PediatricDentistry(request):
     return render(request,'TheKiddoDontist/PediatricDentistry.html')
 def GeneralAnesthesia(request):
     return render(request,'TheKiddoDontist/GeneralAnesthesia.html')
+def contactus(request):
+    return render(request,'TheKiddoDontist/contactus.html')
+def OnlineBooking(request):
+    return render(request,'TheKiddoDontist/OnlineBooking.html')
+def Reviews(request):
+    return render(request,'TheKiddoDontist/Reviews.html')
+def login(request):
+    return render(request,'TheKiddoDontist/login.html')
 
