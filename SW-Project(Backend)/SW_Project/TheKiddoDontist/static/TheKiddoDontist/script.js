6fbbd84bccce596b565a3ae84f7522014318704f
farida-waleed02 <faridawaleed312@hotmail.com>
2023-05-18 14:11:07 +0300

    All Pictures except background and homepage

--- SW-Project(Backend)/SW_Project/TheKiddoDontist/static/TheKiddoDontist/script.js
+++ SW-Project(Backend)/SW_Project/TheKiddoDontist/static/TheKiddoDontist/script.js
@@ -1,6 +1,6 @@
 var images = [
     "best-pediatric-dentist.jpg",
-    "Backtoschool.jpg",
+    "Back-to-School-Dental-Checkup-Pleasant-St-Dental-East-Longmeadow-Ma-e1597064370762.jpg",
     "back3 (2).jpg"
   ];
   
