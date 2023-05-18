5feefb6a4364b84dec2029f2775f45f5e27ecea6
farida-waleed02 <faridawaleed312@hotmail.com>
2023-05-18 11:21:50 +0300

    Javascripts

---
+++ SW-Project(Backend)/SW_Project/TheKiddoDontist/static/TheKiddoDontist/script.js
@@ -0,0 +1,48 @@
+var images = [
+    "best-pediatric-dentist.jpg",
+    "Back-to-School-Dental-Checkup-Pleasant-St-Dental-East-Longmeadow-Ma-e1597064370762.jpg",
+    "back3 (2).jpg"
+  ];
+  
+  var currentIndex = 0;
+  var slideshowElement = document.getElementById("slideshow");
+  
+  function showImage(index) {
+    var image = document.createElement("img");
+    image.src = images[index];
+    image.onload = function() {
+      slideshowElement.appendChild(image);
+      setTimeout(function() {
+        image.style.opacity = 1;
+      }, 100);
+    };
+  }
+  
+  function hideImage(image) {
+    setTimeout(function() {
+      image.style.opacity = 0;
+      setTimeout(function() {
+        slideshowElement.removeChild(image);
+      }, 1000);
+    }, 5000);
+  }
+  
+  showImage(currentIndex);
+  
+  setInterval(function() {
+    var image = slideshowElement.querySelector("img");
+    currentIndex = (currentIndex + 1) % images.length;
+    var nextImage = document.createElement("img");
+    nextImage.src = images[currentIndex];
+    nextImage.onload = function() {
+      slideshowElement.appendChild(nextImage);
+      setTimeout(function() {
+        image.style.opacity = 0;
+        nextImage.style.opacity = 1;
+        setTimeout(function() {
+          slideshowElement.removeChild(image);
+        }, 1000);
+      }, 100);
+    };
+  }, 5000);
+  
